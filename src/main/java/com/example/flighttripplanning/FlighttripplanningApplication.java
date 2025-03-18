b69003785b8f46a78f2673af005d2e28a97fcb5e
karinhenn <karinhenn89@gmail.com>
2025-03-18 15:58:59 +0200

    MockFlights data into json and data from json to postgres

---
+++ src/main/java/com/example/flighttripplanning/FlighttripplanningApplication.java
@@ -0,0 +1,13 @@
+package com.example.flighttripplanning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlighttripplanningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlighttripplanningApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/flighttripplanning/FlighttripplanningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.flighttripplanning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlighttripplanningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
